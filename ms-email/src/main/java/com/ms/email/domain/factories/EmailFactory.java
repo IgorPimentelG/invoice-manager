3e236268fd54650bd5adeba957ff47799fcd9aa4
Igor Pimentel <dev.igorpimentel@gmail.com>
2023-08-29 10:48:41 -0300

    :sparkles: feat: add email factory

---
+++ ms-email/src/main/java/com/ms/email/domain/factories/EmailFactory.java
@@ -0,0 +1,26 @@
+package com.ms.email.domain.factories;
+
+import com.ms.email.domain.entities.Email;
+import com.ms.email.domain.errors.InvalidValueException;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+public abstract class EmailFactory {
+
+	private static final String SENDER = "no-reply@invoice-manager.com";
+
+	public static Email create(String to, String subject, String content, String owner)
+	  throws InvalidValueException {
+
+		Email email = new Email();
+		email.setOwnerRef(owner);
+		email.setFrom(SENDER);
+		email.setTo(to);
+		email.setSubject(subject);
+		email.setContent(content);
+		email.setCreatedAt(LocalDateTime.now().toEpochSecond(ZoneOffset.UTC));
+
+		return email;
+	}
+}
