47b78d0edd64f3b245ef0cd6b58346b8d6636bbe
Igor Pimentel <dev.igorpimentel@gmail.com>
2023-08-29 22:29:01 -0300

    :white_check_mark: test: ensure that be possible send an email

---
+++ ms-email/src/test/java/com/ms/email/infra/mocks/MockEmail.java
@@ -0,0 +1,38 @@
+package com.ms.email.infra.mocks;
+
+import com.ms.email.domain.entities.Email;
+import com.ms.email.domain.types.Status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MockEmail {
+
+	public Email createEntity() throws Exception {
+		return createEntity(0);
+	}
+
+	public List<Email> createListEntity() throws Exception {
+		List<Email> records = new ArrayList<>();
+
+		for (int i = 0; i < 5; i++) {
+			records.add(createEntity(i));
+		}
+
+		return records;
+	}
+
+	public Email createEntity(int index) throws Exception {
+		return new Email(
+		  "dcdafd9b-369a-4c3f-9df6-770c8428ff35",
+		  "72c02d05-feff-4a37-9b91-dbb01cedfa46",
+		  "any_from" + index + "@mail.com",
+		  "any_to" + index + "@mail.com",
+		  "any_subject",
+		  "any_content",
+		  Status.SENT,
+		  1693255833552L
+		);
+	}
+
+}
---
+++ ms-email/src/test/java/com/ms/email/infra/services/EmailServiceTest.java
@@ -0,0 +1,54 @@
+package com.ms.email.infra.services;
+
+import com.ms.email.domain.entities.Email;
+import com.ms.email.domain.types.Status;
+import com.ms.email.infra.mocks.MockEmail;
+import com.ms.email.infra.repositories.EmailRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class EmailServiceTest {
+
+	MockEmail mockEmail;
+
+	@InjectMocks
+	EmailService service;
+
+	@Mock
+	EmailRepository repository;
+
+	@Mock
+	JavaMailSender emailSender;
+
+	@BeforeEach
+	void setup() {
+		mockEmail = new MockEmail();
+		MockitoAnnotations.openMocks(this);
+	}
+
+	@Test
+	@DisplayName("should send an email")
+	void sendEmailTest() throws Exception {
+		Email email = mockEmail.createEntity();
+
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom(email.getFrom());
+		message.setTo(email.getTo());
+		message.setSubject(email.getSubject());
+		message.setText(email.getContent());
+
+		service.send(email);
+
+		assertEquals(email.getStatus(), Status.SENT);
+		verify(emailSender, times(1)).send(message);
+	}
+}
