44f8a489ae92cb3c866d38791387a3998184af6c
Igor Pimentel <dev.igorpimentel@gmail.com>
2023-08-30 11:49:36 -0300

    :fire: chore: remove send email route

--- ms-email/src/main/java/com/ms/email/infra/controllers/EmailController.java
+++ ms-email/src/main/java/com/ms/email/infra/controllers/EmailController.java
@@ -1,16 +1,11 @@
 package com.ms.email.infra.controllers;
 
 import com.ms.email.domain.entities.Email;
-import com.ms.email.domain.errors.InvalidValueException;
 import com.ms.email.domain.errors.NotFoundException;
-import com.ms.email.domain.factories.EmailFactory;
 import com.ms.email.infra.controllers.docs.email.DocFindAllEmailRegisters;
 import com.ms.email.infra.controllers.docs.email.DocFindEmailRegister;
-import com.ms.email.infra.controllers.docs.email.DocSendEmail;
-import com.ms.email.infra.controllers.dtos.EmailDTO;
 import com.ms.email.infra.services.EmailService;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -27,21 +22,6 @@ public class EmailController {
 
 	@Autowired
 	private EmailService service;
-
-	@DocSendEmail
-	@PostMapping("/v1/send")
-	public ResponseEntity<Email> sendEmail(@RequestBody @Valid EmailDTO emailDTO) throws InvalidValueException {
-		Email email = EmailFactory.create(
-		  emailDTO.to(),
-		  emailDTO.subject(),
-		  emailDTO.content(),
-		  emailDTO.ownerRef()
-		);
-
-		var body = service.send(email);
-
-		return ResponseEntity.status(HttpStatus.CREATED).body(body);
-	}
 
 	@DocFindEmailRegister
 	@GetMapping("/v1/{id}")
