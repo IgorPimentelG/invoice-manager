43088de682c8edaf666d089e46018698051a3e04
Igor Pimentel <dev.igorpimentel@gmail.com>
2023-09-01 02:23:58 -0300

    :tada: chore: create ms client

---
+++ ms-client/src/main/java/com/ms/client/main/Application.java
@@ -0,0 +1,18 @@
+package com.ms.client.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan({"com.ms.client.infra", "com.ms.client.main"})
+@EntityScan("com.ms.client.domain.entities")
+@EnableJpaRepositories("com.ms.client.infra.repositories")
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
+
