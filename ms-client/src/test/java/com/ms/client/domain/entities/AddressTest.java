95bbe9eee32beeb9a879c2cff4098ead473cd117
Igor Pimentel <dev.igorpimentel@gmail.com>
2023-09-02 18:02:04 -0300

    :white_check_mark: test: ensure that throws an error when create address with empty state

--- ms-client/src/test/java/com/ms/client/domain/entities/AddressTest.java
+++ ms-client/src/test/java/com/ms/client/domain/entities/AddressTest.java
@@ -1,5 +1,6 @@
 package com.ms.client.domain.entities;
 
+import com.ms.client.domain.errors.IncorrectValueException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -21,5 +22,26 @@ public class AddressTest {
 		);
 
 		assertNotNull(result);
+	}
+
+	@Test
+	@DisplayName("should throws an exception when create a address with empty state")
+	void testCreateAddressWithEmptyState() {
+		Exception exception = assertThrows(IncorrectValueException.class, () -> {
+			var result = new Address(
+			  1L,
+			  "",
+			  "any city",
+			  "any street",
+			  "any neighborhood",
+			  "00-X",
+			  "00000-000"
+			);
+		});
+
+		String expectedMessage = "State cannot be empty.";
+		String resultMessage = exception.getMessage();
+
+		assertEquals(expectedMessage, resultMessage);
 	}
 }
