2909e62261027dbcf06a5f6be3709bef48970a9d
Igor Pimentel <dev.igorpimentel@gmail.com>
2023-09-02 18:12:22 -0300

    :white_check_mark: test: ensure that throws an error when create address with incorrect state format

--- ms-client/src/test/java/com/ms/client/domain/entities/AddressTest.java
+++ ms-client/src/test/java/com/ms/client/domain/entities/AddressTest.java
@@ -1,5 +1,6 @@
 package com.ms.client.domain.entities;
 
+import com.ms.client.domain.errors.FormatException;
 import com.ms.client.domain.errors.IncorrectValueException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -40,6 +41,27 @@ public class AddressTest {
 		});
 
 		String expectedMessage = "State cannot be empty.";
+		String resultMessage = exception.getMessage();
+
+		assertEquals(expectedMessage, resultMessage);
+	}
+
+	@Test
+	@DisplayName("should throws an exception when create a address with incorrect state format")
+	void testCreateAddressWithIncorrectStateFormat() {
+		Exception exception = assertThrows(FormatException.class, () -> {
+			var result = new Address(
+			  1L,
+			  "any state",
+			  "any city",
+			  "any street",
+			  "any neighborhood",
+			  "00-X",
+			  "00000-000"
+			);
+		});
+
+		String expectedMessage = "The state must be only the abbreviation in capital letters.";
 		String resultMessage = exception.getMessage();
 
 		assertEquals(expectedMessage, resultMessage);
