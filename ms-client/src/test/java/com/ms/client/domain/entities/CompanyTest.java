a23b347a3bd2f96754c43b3e9c65a78c40f94063
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2023-09-03 20:54:09 -0300

    :white_check_mark: test: ensure that throws an error when create a company with empty email

--- ms-client/src/test/java/com/ms/client/domain/entities/CompanyTest.java
+++ ms-client/src/test/java/com/ms/client/domain/entities/CompanyTest.java
@@ -148,4 +148,25 @@ public class CompanyTest {
 
 		assertEquals(expectedMessage, resultMessage);
 	}
+
+	@Test
+	@DisplayName("should throw an exception when create a company with email")
+	void testCreateUserManagerWithEmail() {
+		Exception exception = assertThrows(IncorrectValueException.class, () -> {
+			new Company(
+			  "4ff6d786-b7a1-4efc-be85-a297e04c2f48",
+			  "any corporate name",
+			  TaxRegime.SIMPLE_NATIONAL,
+			  "00.000.000/0000-00",
+			  "",
+			  "(00) 00000-0000",
+			  manager
+			);
+		});
+
+		String expectedMessage = "Email must not be empty.";
+		String resultMessage = exception.getMessage();
+
+		assertEquals(expectedMessage, resultMessage);
+	}
 }
