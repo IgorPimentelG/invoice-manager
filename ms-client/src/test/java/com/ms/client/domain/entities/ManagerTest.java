611ae296f6bd7ca5804777a20da320c964327ca7
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2023-09-02 23:02:40 -0300

    :white_check_mark: test: ensure that throws an error when create manager with incorrect id format

--- ms-client/src/test/java/com/ms/client/domain/entities/ManagerTest.java
+++ ms-client/src/test/java/com/ms/client/domain/entities/ManagerTest.java
@@ -1,5 +1,6 @@
 package com.ms.client.domain.entities;
 
+import com.ms.client.domain.errors.FormatException;
 import com.ms.client.domain.errors.IncorrectValueException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -42,6 +43,27 @@ public class ManagerTest {
 		});
 
 		String expectedMessage = "ID must not be empty.";
+		String resultMessage = exception.getMessage();
+
+		assertEquals(expectedMessage, resultMessage);
+	}
+
+	@Test
+	@DisplayName("should throw an exception when create a user manager with incorrect id format")
+	void testCreateUserManagerWithIncorrectIdFormat() {
+		Exception exception = assertThrows(FormatException.class, () -> {
+			new Manager(
+			  "1L",
+			  "000.000.000-00",
+			  "any full name",
+			  LocalDate.parse("1990-01-01"),
+			  "(00) 00000-0000",
+			  "any@mail.com",
+			  "anyPassword0"
+			);
+		});
+
+		String expectedMessage = "ID must be a UUID.";
 		String resultMessage = exception.getMessage();
 
 		assertEquals(expectedMessage, resultMessage);
