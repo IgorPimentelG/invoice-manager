e6466ee047bdfa3630848faf0ed663ce54abb49d
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2023-09-23 23:02:05 -0300

    :memo: docs: add swagger

--- service-auth/src/main/java/com/ms/service/auth/infra/controllers/AuthController.java
+++ service-auth/src/main/java/com/ms/service/auth/infra/controllers/AuthController.java
@@ -1,7 +1,9 @@
 package com.ms.service.auth.infra.controllers;
 
+import com.ms.service.auth.infra.controllers.docs.ApiOperationValidateToken;
 import com.ms.service.auth.infra.dtos.TokenDto;
 import com.ms.service.auth.infra.services.TokenService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
+@Tag(name = "Auth", description = "Endpoints for valid authentication")
 public class AuthController {
 
 	@Autowired
 	private TokenService service;
 
 	@GetMapping("/token/validate")
+	@ApiOperationValidateToken
 	public ResponseEntity<TokenDto> validateToken(@RequestParam("accessToken") String accessToken) {
 		var result = service.validateToken(accessToken);
 		return ResponseEntity.ok(result);
